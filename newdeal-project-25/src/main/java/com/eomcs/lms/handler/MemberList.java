d5a38b872b0a23adce9b1cddd8a07f339c9f0518
goatking91 <tmdals3021@gmail.com>
2018-12-02 12:34:43 +0900

    v34 and homework

---
+++ newdeal-project-25/src/main/java/com/eomcs/lms/handler/MemberList.java
@@ -0,0 +1,36 @@
+package com.eomcs.lms.handler;
+
+import java.util.Arrays;
+import com.eomcs.lms.domain.Member;
+
+public class MemberList {
+  static final int LENGTH = 10;
+  private Member[] list;
+  private int size;
+  
+  public MemberList() {
+    this.list = new Member[LENGTH];
+  }
+  
+  public MemberList(int initialCapacity) {
+    if(initialCapacity > LENGTH) {
+      this.list = new Member[initialCapacity];
+    } else {
+      this.list = new Member[LENGTH];
+    }
+    
+  }
+  
+  public Member[] toArray() {
+    return Arrays.copyOf(list, size);
+  }
+  
+  public void add(Member member) {
+    if(size >= list.length) {
+      int oldLength = list.length;
+      int newCapacity = oldLength + oldLength >> 1;
+      list = Arrays.copyOf(list, newCapacity);
+    }
+    list[size++] = member;
+  }
+}
--- newdeal-project-34/src/main/java/com/eomcs/lms/handler/MemberAddCommand.java
+++ newdeal-project-34/src/main/java/com/eomcs/lms/handler/MemberAddCommand.java
@@ -1,56 +1,47 @@
 package com.eomcs.lms.handler;
+
 import java.sql.Date;
+import java.util.List;
 import java.util.Scanner;
 import com.eomcs.lms.domain.Member;
 
-public class MemberHandler {
-  
-  static final int LENGTH = 10;
-
+public class MemberAddCommand implements Command {
   Scanner keyboard;
-  MemberList list;
-  public MemberHandler(Scanner keyboard) {
+  List<Member> list;
+
+  public MemberAddCommand(Scanner keyboard, List<Member> list) {
     this.keyboard = keyboard;
-    this.list = new MemberList();
-  }
-  
-  public void listMember() {
-    
-    Member[] Members = list.toArray(); 
-    for (Member member : Members) {
-      System.out.printf("%3d, %-4s, %-20s, %-15s, %s\n", 
-          member.getNo(), member.getName(), 
-          member.getEmail(), member.getTel(),
-          member.getRegisteredDate());
-    }
+    this.list = list;
   }
 
-  public void addMember() {
+  @Override
+  public void execute() {
     Member member = new Member();
-    
+
     System.out.print("번호? ");
     member.setNo(Integer.parseInt(keyboard.nextLine()));
-    
+
     System.out.print("이름? ");
     member.setName(keyboard.nextLine());
-    
+
     System.out.print("이메일? ");
     member.setEmail(keyboard.nextLine());
-    
+
     System.out.print("암호? ");
     member.setPassword(keyboard.nextLine());
-  
+
     System.out.print("사진? ");
     member.setPhoto(keyboard.nextLine());
-  
+
     System.out.print("전화? ");
     member.setTel(keyboard.nextLine());
-  
+
     member.setRegisteredDate(new Date(System.currentTimeMillis())); 
-    
+
     list.add(member);
-    
+
     System.out.println("저장하였습니다.");
+
   }
 
 }
